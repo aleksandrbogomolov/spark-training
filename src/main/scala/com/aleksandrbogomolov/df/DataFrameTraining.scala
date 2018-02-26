da4a2b2aa51b1c5edcc29e908953c221eae55df1
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2018-02-26 21:46:49 +0300

    Changed raw data to the movies dataset from grouplens.org. Refactored for get dataframe from new sources

--- src/main/scala/com/aleksandrbogomolov/df/DataFrameTraining.scala
+++ src/main/scala/com/aleksandrbogomolov/df/DataFrameTraining.scala
@@ -6,29 +6,36 @@ import org.apache.spark.sql.types._
 object DataFrameTraining extends App {
 
   val spark = SparkSession.builder().appName("training").config("spark.master", "local").getOrCreate()
-  val initDF = readSrc("/Users/aleksandrbogomolov/Documents/Spark/thads2013n.txt")
+  val moviesDF = readSrc("/Users/aleksandrbogomolov/Documents/Spark/ml-latest/movies.csv")
+  val ratingsDF = readSrc("/Users/aleksandrbogomolov/Documents/Spark/ml-latest/ratings.csv")
 
   def readSrc(path: String): DataFrame = {
     val rdd = spark.sparkContext.textFile(path)
     val columns = rdd.first().split(",").toList
     val schema = dfSchema(columns)
-
-    val rows = rdd
-      .mapPartitionsWithIndex((i, it) => if (i == 0) it.drop(1) else it)
-      .map(_.split(",").toList)
-      .map(row)
-
+    val rows = rdd.mapPartitionsWithIndex((i, it) => if (i == 0) it.drop(1) else it).map(splitRow(_).toList).map(row)
     spark.createDataFrame(rows, schema)
   }
 
   def dfSchema(columns: List[String]): StructType = {
-    StructType(StructField(columns.head, StringType, nullable = false) :: columns.tail.map(StructField(_, StringType,
-      nullable = false)))
+    StructType(StructField(columns.head, StringType, nullable = false)
+      :: columns.tail.map(StructField(_, StringType, nullable = false)))
   }
+
+  def splitRow(line: String): Array[String] = {
+    if (line.contains("\"")) {
+      line.split("\"").map(removeComa)
+    } else {
+      line.split(",")
+    }
+  }
+
+  def removeComa(word: String): String = if (word.startsWith(",") || word.endsWith(",")) word.replace(",", "") else word
 
   def row(list: List[String]): Row = {
     Row(list.head.toString :: list.tail.map(_.toString): _*)
   }
 
-  initDF.show()
+  moviesDF.show()
+  ratingsDF.show()
 }
